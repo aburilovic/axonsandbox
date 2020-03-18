41b2c15cd60d17763d8fdff2d1dce76eef93c38b
Ante Burilovic <ante.burilovic.dev@gmail.com>
2020-03-18 10:21:08 +0100

    initial folder structure

---
+++ axonsandbox/src/main/java/com/bluedrop/axonsandbox/AxonsandboxApplication.java
@@ -0,0 +1,13 @@
+package com.bluedrop.axonsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonsandboxApplication.class, args);
+	}
+
+}
---
+++ axonsandbox/src/test/java/com/bluedrop/axonsandbox/AxonsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedrop.axonsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
